c38c493092df0206754265382641942012804940
Janghyun Son <jhson989@gmail.com>
2022-09-23 14:51:58 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 ./gradlew clean build jar -Pjasypt.encryptor.password=${JAVA_KEY}
-java -jar build/libs/book-0.0.1-SNAPSHOT.jar --jasypt.encryptor.password=${JAVA_KEY}
+java -jar build/libs/book-0.0.1.jar --jasypt.encryptor.password=${JAVA_KEY}
